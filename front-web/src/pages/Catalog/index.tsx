ec99fed4aaebac2bb195edf59b4eca77cf400b42
José Milton <j1000tonm.jmb@gmail.com>
2021-03-12 17:12:10 -0300

    Finish catalog loaders

--- front-web/src/pages/Catalog/index.tsx
+++ front-web/src/pages/Catalog/index.tsx
@@ -1,21 +1,25 @@
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
-import { ProductsResponse } from '../../core/types/Product';
-import { makeRequest } from '../../core/utils/request';
+import { ProductsResponse } from 'core/types/Product';
+import { makeRequest } from 'core/utils/request';
 import ProductCard from './components/ProductCard';
+import ProductCardLoader from './components/Loaders/ProductCardLoader';
 import './styles.scss';
 
 const Catolog = () => {
     const [productsResponse, setProductsResponse] = useState<ProductsResponse>();
-    
+    const [isLoading, setIsLoading] = useState(false);
+
     useEffect(() => {
         const params = {
-            page:0,
             linesPerPage: 12
         }
-
+        setIsLoading(true);
         makeRequest({ url: '/products', params })
-        .then(response => setProductsResponse(response.data));
+        .then(response => setProductsResponse(response.data))
+        .finally(() => {
+            setIsLoading(false);
+        })
     }, []);
 
     return (
@@ -23,12 +27,14 @@ const Catolog = () => {
             <h1 className="catalog-title">
                 Catálogo de produtos
             </h1>
-            <div className="catalog-products">
-                {productsResponse?.content.map(product => (
-                    <Link to={`/products/${product.id}`} key={product.id}>
-                        <ProductCard product={product} />
-                    </Link>
-                ))}
+            <div className="catalog-products"> 
+                {isLoading ? <ProductCardLoader /> : (
+                    productsResponse?.content.map(product => (
+                        <Link to={`/products/${product.id}`} key={product.id}>
+                            <ProductCard product={product} />
+                        </Link>
+                    ))
+                )}
             </div>
         </div>
     )
