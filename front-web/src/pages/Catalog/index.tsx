a5d04da9ee7c8f01dc2aead8135f1b3ac05313b3
José Milton <j1000tonm.jmb@gmail.com>
2021-04-07 20:08:26 -0300

    Add Admin navbar

--- front-web/src/pages/Catalog/index.tsx
+++ front-web/src/pages/Catalog/index.tsx
@@ -5,13 +5,16 @@ import { makeRequest } from 'core/utils/request';
 import ProductCard from './components/ProductCard';
 import ProductCardLoader from './components/Loaders/ProductCardLoader';
 import './styles.scss';
+import Pagination from 'core/components/Pagination';
 
 const Catolog = () => {
     const [productsResponse, setProductsResponse] = useState<ProductsResponse>();
     const [isLoading, setIsLoading] = useState(false);
+    const [activePage, setActivePage] = useState(0);
 
     useEffect(() => {
         const params = {
+            page: activePage,
             linesPerPage: 12
         }
         setIsLoading(true);
@@ -20,7 +23,7 @@ const Catolog = () => {
         .finally(() => {
             setIsLoading(false);
         })
-    }, []);
+    }, [activePage]);
 
     return (
         <div className="catalog-container">
@@ -36,6 +39,13 @@ const Catolog = () => {
                     ))
                 )}
             </div>
+            {productsResponse && (
+                <Pagination 
+                    totalPages={productsResponse.totalPages} 
+                    activePage={activePage}
+                    onChange={page => setActivePage(page)} 
+                />
+            )}
         </div>
     )
 };
